ec4769210e1bf7c6fa5a35b64b21a81eb6557206
Jacky Z <yokiming1994@gmail.com>
2024-01-16 18:34:32 +0800

    feat: change image

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -2,7 +2,7 @@ const projectsData = [
   {
     title: 'placeholder',
     description: `placeholder..`,
-    imgSrc: '/static/images/projects/mountain/banner.png',
+    imgSrc: '/static/images/projects/mountain/banner.jpg',
     href: 'https://github.com/zhenghangjie',
   },
 ]
