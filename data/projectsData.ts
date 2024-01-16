3ad243f6a853340600a8fecae1f5f44f595514e3
zhenghangjie <zhenghangjie@mistong.com>
2024-01-16 18:29:03 +0800

    projectsData

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,11 +1,10 @@
 const projectsData = [
-  // {
-  //   title: 'neujson',
-  //   description: `neujson is a JSON parser and generator for C++. 
-  //   It supports both SAX and DOM style API.`,
-  //   imgSrc: '/static/images/projects/neujson/banner.png',
-  //   href: 'https://github.com/hominsu/neujson',
-  // }
+  {
+    title: 'placeholder',
+    description: `placeholder..`,
+    imgSrc: '/static/images/projects/mountain/banner.png',
+    href: 'https://github.com/zhenghangjie',
+  },
 ]
 
 export default projectsData
